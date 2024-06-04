6b792fda475ea553d8b86e208400ec3a724592b9
BuildTools <ragnaroksmp32@gmail.com>
2024-06-03 23:57:56 -0400

    Added config option for giving pearls on join and option for pearls to not be re useable

--- src/main/java/me/mike3132/pearlrider/EventHandler/LoginEvent.java
+++ src/main/java/me/mike3132/pearlrider/EventHandler/LoginEvent.java
@@ -2,7 +2,6 @@ package me.mike3132.pearlrider.EventHandler;
 
 import me.mike3132.pearlrider.Items.PearlItem;
 import me.mike3132.pearlrider.Main;
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
